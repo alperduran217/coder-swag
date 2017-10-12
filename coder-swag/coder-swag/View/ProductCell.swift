37b05fea01cb4252b4b2131fe5d2152c1c2816e5
alperduran217 <alperduran217@gmail.com>
2017-10-12 15:18:21 +0200

    third lesson done

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Mehmet Alper Duran on 12.10.2017.
+//  Copyright © 2017 Mehmet Alper Duran. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
